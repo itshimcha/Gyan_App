b99c3fd60c89a565a23a90e9c9464fe13cd7d0c2
Himcha <himanshu.chaurasia982006@gmail.com>
2026-07-07 00:55:42 +0530

    Remove unused _buildMessageState widget code
    Removed commented-out _buildMessageState widget code.

--- lib/extra/com_wid.dart
+++ lib/extra/com_wid.dart
@@ -143,26 +143,3 @@ class MainTxt extends StatelessWidget {
   }
 }
 
-// Widget _buildMessageState({required IconData icon, required String message, bool showRetry = false, Function()? ontap}) {
-//   return ListView(
-//     shrinkWrap: true,
-//     physics: NeverScrollableScrollPhysics(),
-//     children: [
-//       const SizedBox(height: 120),
-//       Icon(icon, size: 56, color: Colors.grey),
-//       const SizedBox(height: 12),
-//       Text(message, textAlign: TextAlign.center, style: const TextStyle(color: Colors.grey, fontSize: 15)),
-//       if (showRetry) ...[
-//         const SizedBox(height: 16),
-//         Center(
-//           child: OutlinedButton.icon(
-//             onPressed: (){wi},
-//             icon: const Icon(Icons.refresh),
-//             label: const Text('Retry'),
-//           ),
-//         ),
-//       ],
-//     ],
-//   );
-// }
-
